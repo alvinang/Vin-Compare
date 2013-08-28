2b144f38e47baaf6e9e102896a8183ce8aab4988
Alvin Ang <alvinangbs@gmail.com>
2013-08-27 21:55:22 -0500

    fixed css

--- app/assets/javascripts/vins.js
+++ app/assets/javascripts/vins.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
   var table = $('table');
   var progress = $('.progress');
   table.hide();
-  progress.addClass("loading").delay(6500).queue(function(){$(this).removeClass("loading")})
-  table.show(6000);
-
+  progress.addClass("loading").delay(4800).queue(function(){$(this).removeClass("loading")})
+  table.show(4000);
 });
